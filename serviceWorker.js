4e5693f5e85f408625e291da10fca5905a459b93
PouRiaBG <pooria.bonyadgozar@gmail.com>
2022-07-20 22:38:26 +0430

    feat(): cache files with service workers

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,15 @@
-console.log("Hello world from service worker!")
+const assets = [
+  "/",
+  "js/app.js",
+  "js/sw-register.js",
+  "css/fonts.css",
+  "css/reset.css",
+  "css/style.css",
+];
 
-
+const handleCacheData = () => {
+  caches.open("assets").then(function (cache) {
+    cache.addAll(assets);
+  });
+};
+self.addEventListener("install", handleCacheData);
