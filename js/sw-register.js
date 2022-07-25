bf570ae4b720dd1b118a1106d88dbecd63319bb2
PouRiaBG <pooria.bonyadgozar@gmail.com>
2022-07-25 19:42:10 +0430

    chore(): remove console.log

--- js/sw-register.js
+++ js/sw-register.js
@@ -1,8 +1,10 @@
-if ('serviceWorker' in navigator) {
-    console.log('Browser supports service worker (log on main thread)')
-    navigator.serviceWorker.register('serviceWorker.js').then(function (res) {
-        console.log(res)
-    }).catch((error) => {
-        console.log({ error })
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("serviceWorker.js")
+    .then(function (res) {
+      console.log(res);
     })
+    .catch((error) => {
+      console.log({ error });
+    });
 }
